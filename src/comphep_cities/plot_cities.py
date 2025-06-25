c0d02e63910ed6de07e731519833453f13afd014
Daniel Lewis Humphreys <dahumphr@login29.chn.perlmutter.nersc.gov>
2025-06-25 11:22:55 -0700

    initialized package

--- src/comphep_cities/plot_cities.py
+++ src/comphep_cities/plot_cities.py
@@ -60,7 +60,8 @@ map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
         projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
 
 map.drawstates()
-
+map.drawcountries()
+map.drawcoastlines()
 
 def main():
     # Get the location of each city and plot it
